117c60da8d0bafbf876bca8caf5233c423d5eef6
fuping.chu <fuping.chu@shopee.com>
2024-12-19 13:31:18 +0000

    up dataloader

--- image_datasets/dataset_processed.py
+++ image_datasets/dataset_processed.py
@@ -2,7 +2,7 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 # from dataset import CustomImageDataset
-import random
+from prefetch_generator import BackgroundGenerator
 
 class CustomImageDatasetProcessed(Dataset):
     def __init__(self, img_dir, **kwargs):
@@ -10,7 +10,6 @@ class CustomImageDatasetProcessed(Dataset):
         pt_dir = img_dir + '_processed'
         self.pt_files = [os.path.join(pt_dir, i) for i in os.listdir(pt_dir) if '.pt' in i]
         self.pt_files.sort()
-        self.cache = {} 
         # super().__init__(img_dir, **kwargs)
 
     def __len__(self):
@@ -19,14 +18,9 @@ class CustomImageDatasetProcessed(Dataset):
     def __getitem__(self, idx, retries=3):
         while retries > 0:
             try:
-                if idx in self.cache:  
-                    data = self.cache[idx]  
-                    return data["img"], data["img_ids"], data["txt"], data["txt_ids"], data["vec"]
-                else:
-                    pt_file = self.pt_files[idx]
-                    data = torch.load(pt_file, weights_only=True)
-                    self.cache[idx] = data 
-                    return data["img"], data["img_ids"], data["txt"], data["txt_ids"], data["vec"]
+                pt_file = self.pt_files[idx]
+                data = torch.load(pt_file, weights_only=True)
+                return data["img"], data["img_ids"], data["txt"], data["txt_ids"], data["vec"]
 
             except Exception as e:
                 print(f"Error loading file {pt_file}: {e}")
@@ -34,11 +28,15 @@ class CustomImageDatasetProcessed(Dataset):
                 return self.__getitem__((idx + 1) % len(self.pt_files), retries)
 
         raise Exception(f"Failed to load file after {retries} attempts.")
-
+    
+class DataLoaderProcessed(DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__(), max_prefetch=2)
+    
 def loader(train_batch_size, num_workers, **args):
     # torch.multiprocessing.set_start_method('spawn', force=True)
     dataset = CustomImageDatasetProcessed(**args)
-    return DataLoader(dataset, 
+    return DataLoaderProcessed(dataset, 
                       batch_size=train_batch_size, 
                       num_workers=num_workers, 
                       shuffle=True,
